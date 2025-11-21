d3920f9604d73c46089b1cfced12a9089e9d0f06
jtrynz <John.trynz@gmx.de>
2025-11-21 20:57:45 +0100

    Fix Lottie injection

--- login_screen.py
+++ login_screen.py
@@ -1,3 +1,36 @@
+import streamlit as st
+
+VALID_CREDENTIALS = {
+    "admin": "evaluera2024",
+    "demo": "demo123",
+    "user": "password"
+}
+
+
+def check_login():
+    """Return True if user is logged in."""
+    if "logged_in" not in st.session_state:
+        st.session_state.logged_in = False
+    return st.session_state.logged_in
+
+
+def login(username, password):
+    """Validate login credentials."""
+    if username in VALID_CREDENTIALS and VALID_CREDENTIALS[username] == password:
+        st.session_state.logged_in = True
+        st.session_state.username = username
+        return True
+    return False
+
+
+def logout():
+    """Logout user."""
+    st.session_state.logged_in = False
+    if "username" in st.session_state:
+        del st.session_state.username
+    st.rerun()
+
+
 def render_login_screen():
     """
     Render simple login screen without custom HTML/CSS
@@ -33,3 +66,9 @@ def render_login_screen():
         else:
             st.session_state.login_error = True
             st.rerun()
+
+def render_logout_button():
+    with st.sidebar:
+        st.write("Eingeloggt als:", st.session_state.get("username", "Benutzer"))
+        if st.button("Abmelden"):
+            logout()
